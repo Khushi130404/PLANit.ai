8863f282801aae497ec64bb361d78a71053190d2
Arup <aruptarafdar1@gmail.com>
2025-01-31 15:25:43 +0530

    Added Firebase Login fully

---
+++ app/src/main/java/com/planzy/planzyai/ActivityLogin.java
@@ -0,0 +1,137 @@
+package com.planzy.planzyai;
+
+import android.content.Intent;
+import android.content.IntentSender;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.identity.BeginSignInResult;
+import com.google.android.gms.auth.api.identity.Identity;
+import com.google.android.gms.auth.api.identity.SignInClient;
+import com.google.android.gms.auth.api.identity.SignInCredential;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class ActivityLogin extends AppCompatActivity {
+    MaterialButton login_button;
+    private static final int REQ_ONE_TAP = 2;  // Can be any integer unique to the Activity.
+    private static final int LOGIN_CLOSED = 0;  // 0 = Closed popup without login
+    private SignInClient oneTapClient;
+    private BeginSignInRequest signUpRequest;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        login_button = findViewById(R.id.login_button);
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                oneTapClient = Identity.getSignInClient(ActivityLogin.this);
+                signUpRequest = BeginSignInRequest.builder()
+                        .setGoogleIdTokenRequestOptions(BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                                .setSupported(true)
+                                // Your server's client ID, not your Android client ID.
+                                .setServerClientId("149935574804-vrhqio35bqet4kch1lv4240re3u37rma.apps.googleusercontent.com")
+                                // Show all accounts on the device.
+                                .setFilterByAuthorizedAccounts(false)
+                                .build())
+                        .build();
+
+                oneTapClient.beginSignIn(signUpRequest).addOnSuccessListener(new OnSuccessListener<BeginSignInResult>() {
+                    @Override
+                    public void onSuccess(BeginSignInResult result) {
+                        try {
+                            startIntentSenderForResult(
+                                    result.getPendingIntent().getIntentSender(), REQ_ONE_TAP,
+                                    null, 0, 0, 0);
+                        } catch (IntentSender.SendIntentException e) {
+                            Toast.makeText(ActivityLogin.this, "Can't load sign in", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        System.out.println(e);
+                    }
+                });
+
+            }
+        });
+
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        System.out.println("RESULT CODE IS: " + resultCode);
+        if (resultCode == LOGIN_CLOSED){
+            System.out.println("Login window closed");
+        }
+        else if (requestCode == REQ_ONE_TAP){
+            Toast.makeText(ActivityLogin.this, "DONE!", Toast.LENGTH_SHORT).show();
+            try {
+
+                SignInCredential credential = oneTapClient.getSignInCredentialFromIntent(data);
+                String idToken = credential.getGoogleIdToken();
+                DatabaseReference userreference = FirebaseDatabase.getInstance().getReference("User").push();
+                if (idToken !=  null) {
+                    // Got an ID token from Google. Use it to authenticate
+                    // with Firebase.
+                    AuthCredential firebaseCredential = GoogleAuthProvider.getCredential(idToken, null);
+                    FirebaseAuth.getInstance().signInWithCredential(firebaseCredential)
+                            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        userreference.child("Name").setValue(FirebaseAuth.getInstance().getCurrentUser().getDisplayName());
+                                        Intent intent = new Intent(ActivityLogin.this, MainActivity.class);
+                                        startActivity(intent);
+                                        ActivityLogin.this.finish();
+                                        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                                    } else {
+                                        Toast.makeText(ActivityLogin.this, "Sign in failed", Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            });
+                }
+                else{
+                    Toast.makeText(ActivityLogin.this, "ID TOKEN PROBLEM", Toast.LENGTH_LONG).show();
+                }
+            } catch (ApiException e) {
+                Toast.makeText(ActivityLogin.this, "Sign in failed", Toast.LENGTH_LONG).show();
+                // ...
+            }
+        }
+    }
+}
--- app/src/main/java/com/planzy/planzyai/MainActivity.java
+++ app/src/main/java/com/planzy/planzyai/MainActivity.java
@@ -1,5 +1,6 @@
 package com.planzy.planzyai;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,6 +8,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +23,11 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        if(FirebaseAuth.getInstance().getUid() == null){
+            Intent intent = new Intent(MainActivity.this, ActivityLogin.class);
+            startActivity(intent);
+            MainActivity.this.finish();
+        }
     }
 }
